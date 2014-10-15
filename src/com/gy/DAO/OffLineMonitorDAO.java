ec76c8ffa1daa3e7f538ee899becccee712ba4ab
linym <linym3k@126.com>
2014-10-15 20:30:15 +0800

    v

--- src/com/gy/DAO/OffLineMonitorDAO.java
+++ src/com/gy/DAO/OffLineMonitorDAO.java
@@ -2,7 +2,11 @@ package com.gy.DAO;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+
+import com.gy.CarMonitor.DBHelper;
+import com.gy.Entity.DBObjectEntity;
 
 public class OffLineMonitorDAO {
 	String sql = "";
@@ -12,10 +16,24 @@ public class OffLineMonitorDAO {
 	Connection conn;
 
 	public void getOffLineDetail(){
+		conn  = new DBHelper().getConn(); 
+		sql="select ter_id,on_off_flag,create_time from sa.tbl_s_terminal_online where trunc(gtime(create_time))= trunc(sysdate)-1 order by ter_id,id asc ";
+		try {
+			stat = conn.createStatement();
+//			System.err.println(sql);
+			rs = stat.executeQuery(sql);
+			while (rs.next()) {
+				DBObjectEntity obj = new DBObjectEntity();  
+				System.err.println(rs.getString("ter_id"));
+//				System.err.println(obj); 
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	public OffLineMonitorDAO() {
-		// TODO Auto-generated constructor stub
+		 
 	}
 
 	/**
@@ -23,7 +41,8 @@ public class OffLineMonitorDAO {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		OffLineMonitorDAO off = new OffLineMonitorDAO();
+		off.getOffLineDetail();
 	}
 
 }
